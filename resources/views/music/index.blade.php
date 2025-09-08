67a790b703507b103f3636d70fe11ba30967faf7
nato-re <alvares.renato@hotmail.com>
2025-09-08 18:38:03 -0300

    setup env

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -28,12 +28,6 @@
                     <td>{{ $item->genre }}</td>
                     <td>
                         <a href="{{ route('music.show', $item) }}" class="btn btn-info btn-sm">View</a>
-                        <a href="{{ route('music.edit', $item) }}" class="btn btn-warning btn-sm">Edit</a>
-                        <form action="{{ route('music.destroy', $item) }}" method="POST" style="display:inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
                     </td>
                 </tr>
             @endforeach
